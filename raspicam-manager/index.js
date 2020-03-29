95b745af94390a0284de71b22860ff1b87906d30
Christopher Stevens <owntheweb@gmail.com>
2020-03-29 01:47:31 -0600

    Setup TypeScript and ESLint

--- raspicam-manager/index.js
+++
@@ -1,5 +0,0 @@
-// TEMP: upload to S3 https://stackabuse.com/uploading-files-to-aws-s3-with-node-js/
-
-setInterval(() => {
-  console.log('made it this far!');
-}, 5000);
---
+++ raspicam-manager/src/server.ts
@@ -0,0 +1,3 @@
+setInterval(() => {
+  console.log("Moving along!");
+}, 5000);
